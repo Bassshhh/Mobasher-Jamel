1c61bbd2aae204df0f79bf8b2f0280bb02137627
Bassshhh <32511203+Bassshhh@users.noreply.github.com>
2017-12-29 23:37:22 +0000

    Add files via upload

---
+++ Ctec3905/login.js
@@ -0,0 +1,9 @@
+// Get the modal
+var modal = document.getElementById('id1');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == loginform) {
+        modal.style.display = "none";
+    }
+}
